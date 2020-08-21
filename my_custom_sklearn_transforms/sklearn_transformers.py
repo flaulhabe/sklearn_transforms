ad199fd73dd87efe5700572b26b548ec3a283a27
flaulhabe <69877699+flaulhabe@users.noreply.github.com>
2020-08-21 11:30:39 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,20 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
+
+ class DataFrameImputer(TransformerMixin):
+    def __init__(self):
+        """ Colunas no formato dtype são substituídas pelo valor mais frequente na coluna
+            Colunas de outros tipos são substituídas com a média dos valores da coluna
+            O objetivo é evitar erros envolvendo diferentes formatos na hora de substituir os valores
+        """
+    def fit(self, X, y=None):
+
+        self.fill = pd.Series([X[c].value_counts().index[0]
+            if X[c].dtype == np.dtype('O') else X[c].mean() for c in X],
+            index=X.columns)
+        return self
+
+    def transform(self, X, y=None):
+        return X.fillna(self.fill)
